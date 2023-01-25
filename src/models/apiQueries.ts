cfe6bf4380f69a1a5cc21a5e41ffaf6308b63572
Hiroji <joegilbertdev@outlook.com>
2023-01-25 18:17:53 +0000

    apiQueries.ts: fixed reversed lat long bug

--- src/models/apiQueries.ts
+++ src/models/apiQueries.ts
@@ -11,20 +11,23 @@ export const fetchLocation = async (location: string) => {
       `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
     );
     const data = await rawData.json();
+    console.log(data.results)
     return data.results ;
   } catch (error) {
     console.log(error);
   }
 };
 
-export const fetchCurrentWeather = async (longitude: number, latitude: number) => {
+export const fetchCurrentWeather = async (latitude: number, longitude: number) => {
   try {
-
     //daily https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max&timezone=auto&daily=temperature_2m_min&daily=weathercode&daily=precipitation_sum
     const rawData = await fetch(
       `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
     );
     const data = await rawData.json();
+    if (!data.current_weather) {
+      console.log(data.error, data.reason)
+    }
     return data.current_weather;
   } catch (error) {
     console.log(error);
