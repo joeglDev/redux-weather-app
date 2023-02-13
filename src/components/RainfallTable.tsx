e48920164930d3a08c10aa52977accb4c05dd83e
Hiroji <joegilbertdev@outlook.com>
2023-02-13 17:15:02 +0000

    RainfallTable.tsx: added html for table

--- src/components/RainfallTable.tsx
+++ src/components/RainfallTable.tsx
@@ -13,9 +13,55 @@ export const RainfallTable = () => {
   const currentRainfall = useAppSelector((state: RootState) => {
     return state.rainfall;
   });
+
+  //variables
+  const labels = [
+    "00:00",
+    "01:00",
+    "02:00",
+    "03:00",
+    "04:00",
+    "05:00",
+    "06:00",
+    "07:00",
+    "08:00",
+    "09:00",
+    "10:00",
+    "11:00",
+    "12:00",
+    "13:00",
+    "14:00",
+    "15:00",
+    "16:00",
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+    "24:00",
+  ];
+
     return (
         <section>
             <h3 className="RainfallTable__h3">{"Hourly rainfall (tabular)"}</h3>
+            <table>
+                <caption>Hourly rainfall of the day in mm of rain per hour.</caption>
+  <tr>
+    {labels.map((label) => {
+        return (
+            <th>{label}</th>
+        )
+    })}
+  </tr>
+  <tr>
+    {currentRainfall.map((data) => {
+        return (
+            <td>{data}</td>
+        )
+    })}
+  </tr>
+  </table>
         </section>
     )
 }
